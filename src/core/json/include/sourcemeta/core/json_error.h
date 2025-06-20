aef42402c9506570b2cbcb1b290ebb771efae7f4
Juan Cruz Viotti <jv@jviotti.com>
2025-06-20 12:49:21 -0400

    `core-the-jsonfileparseerror-exception-always-copies-the-path-when-using-the-path-method-adversarial-1`
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/core/json/include/sourcemeta/core/json_error.h
+++ src/core/json/include/sourcemeta/core/json_error.h
@@ -82,7 +82,8 @@ public:
         path_{path} {}
 
   /// Get the file path of the error
-  [[nodiscard]] auto path() const noexcept -> const std::filesystem::path {
+  [[nodiscard]] auto path() const noexcept
+      -> std::remove_reference_t<const std::filesystem::path &> {
     return path_;
   }
 
