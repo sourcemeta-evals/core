7f402e2450357feccb85b5ecadf403f26bbca4ca
Staging-Devin AI <166158716+staging-devin-ai-integration[bot]@users.noreply.github.com>
2025-12-17 10:40:00 +0000

    Add std::hash specializations for Pointer and WeakPointer
    Enable use of Pointer and WeakPointer with std::unordered_map and
    std::unordered_set by providing std::hash specializations.
    
    The hash implementation is O(1) by sampling first, middle, and last
    tokens. For property tokens, it uses the first member of the
    pre-computed property hash. For index tokens, it uses the index value
    directly.
    
    Co-Authored-By: unknown <>

---
+++ test/jsonpointer/jsonpointer_hash_test.cc
@@ -0,0 +1,179 @@
+#include <gtest/gtest.h>
+
+#include <sourcemeta/core/jsonpointer.h>
+
+#include <unordered_map>
+#include <unordered_set>
+
+TEST(JSONPointer_hash, pointer_empty) {
+  const sourcemeta::core::Pointer pointer;
+  const std::hash<sourcemeta::core::Pointer> hasher;
+  EXPECT_EQ(hasher(pointer), 0);
+}
+
+TEST(JSONPointer_hash, pointer_single_property) {
+  const sourcemeta::core::Pointer pointer{"foo"};
+  const std::hash<sourcemeta::core::Pointer> hasher;
+  EXPECT_NE(hasher(pointer), 0);
+}
+
+TEST(JSONPointer_hash, pointer_single_index) {
+  const sourcemeta::core::Pointer pointer{5};
+  const std::hash<sourcemeta::core::Pointer> hasher;
+  EXPECT_NE(hasher(pointer), 0);
+}
+
+TEST(JSONPointer_hash, pointer_multiple_properties) {
+  const sourcemeta::core::Pointer pointer{"foo", "bar", "baz"};
+  const std::hash<sourcemeta::core::Pointer> hasher;
+  EXPECT_NE(hasher(pointer), 0);
+}
+
+TEST(JSONPointer_hash, pointer_multiple_indices) {
+  const sourcemeta::core::Pointer pointer{0, 1, 2};
+  const std::hash<sourcemeta::core::Pointer> hasher;
+  EXPECT_NE(hasher(pointer), 0);
+}
+
+TEST(JSONPointer_hash, pointer_mixed) {
+  const sourcemeta::core::Pointer pointer{"foo", 1, "bar"};
+  const std::hash<sourcemeta::core::Pointer> hasher;
+  EXPECT_NE(hasher(pointer), 0);
+}
+
+TEST(JSONPointer_hash, pointer_equal_pointers_same_hash) {
+  const sourcemeta::core::Pointer pointer_1{"foo", "bar", "baz"};
+  const sourcemeta::core::Pointer pointer_2{"foo", "bar", "baz"};
+  const std::hash<sourcemeta::core::Pointer> hasher;
+  EXPECT_EQ(hasher(pointer_1), hasher(pointer_2));
+}
+
+TEST(JSONPointer_hash, pointer_different_pointers_different_hash) {
+  const sourcemeta::core::Pointer pointer_1{"foo", "bar"};
+  const sourcemeta::core::Pointer pointer_2{"baz", "qux"};
+  const std::hash<sourcemeta::core::Pointer> hasher;
+  EXPECT_NE(hasher(pointer_1), hasher(pointer_2));
+}
+
+TEST(JSONPointer_hash, pointer_unordered_set) {
+  std::unordered_set<sourcemeta::core::Pointer> pointers;
+  pointers.insert(sourcemeta::core::Pointer{"foo"});
+  pointers.insert(sourcemeta::core::Pointer{"bar"});
+  pointers.insert(sourcemeta::core::Pointer{"foo"});
+  EXPECT_EQ(pointers.size(), 2);
+  EXPECT_TRUE(pointers.contains(sourcemeta::core::Pointer{"foo"}));
+  EXPECT_TRUE(pointers.contains(sourcemeta::core::Pointer{"bar"}));
+}
+
+TEST(JSONPointer_hash, pointer_unordered_map) {
+  std::unordered_map<sourcemeta::core::Pointer, int> map;
+  map[sourcemeta::core::Pointer{"foo"}] = 1;
+  map[sourcemeta::core::Pointer{"bar"}] = 2;
+  map[sourcemeta::core::Pointer{"foo"}] = 3;
+  EXPECT_EQ(map.size(), 2);
+  EXPECT_EQ(map[sourcemeta::core::Pointer{"foo"}], 3);
+  EXPECT_EQ(map[sourcemeta::core::Pointer{"bar"}], 2);
+}
+
+TEST(JSONPointer_hash, weakpointer_empty) {
+  const sourcemeta::core::WeakPointer pointer;
+  const std::hash<sourcemeta::core::WeakPointer> hasher;
+  EXPECT_EQ(hasher(pointer), 0);
+}
+
+TEST(JSONPointer_hash, weakpointer_single_property) {
+  const std::string foo = "foo";
+  const sourcemeta::core::WeakPointer pointer{std::cref(foo)};
+  const std::hash<sourcemeta::core::WeakPointer> hasher;
+  EXPECT_NE(hasher(pointer), 0);
+}
+
+TEST(JSONPointer_hash, weakpointer_single_index) {
+  const sourcemeta::core::WeakPointer pointer{5};
+  const std::hash<sourcemeta::core::WeakPointer> hasher;
+  EXPECT_NE(hasher(pointer), 0);
+}
+
+TEST(JSONPointer_hash, weakpointer_multiple_properties) {
+  const std::string foo = "foo";
+  const std::string bar = "bar";
+  const std::string baz = "baz";
+  const sourcemeta::core::WeakPointer pointer{std::cref(foo), std::cref(bar),
+                                              std::cref(baz)};
+  const std::hash<sourcemeta::core::WeakPointer> hasher;
+  EXPECT_NE(hasher(pointer), 0);
+}
+
+TEST(JSONPointer_hash, weakpointer_multiple_indices) {
+  const sourcemeta::core::WeakPointer pointer{0, 1, 2};
+  const std::hash<sourcemeta::core::WeakPointer> hasher;
+  EXPECT_NE(hasher(pointer), 0);
+}
+
+TEST(JSONPointer_hash, weakpointer_mixed) {
+  const std::string foo = "foo";
+  const std::string bar = "bar";
+  const sourcemeta::core::WeakPointer pointer{std::cref(foo), 1,
+                                              std::cref(bar)};
+  const std::hash<sourcemeta::core::WeakPointer> hasher;
+  EXPECT_NE(hasher(pointer), 0);
+}
+
+TEST(JSONPointer_hash, weakpointer_equal_pointers_same_hash) {
+  const std::string foo = "foo";
+  const std::string bar = "bar";
+  const std::string baz = "baz";
+  const sourcemeta::core::WeakPointer pointer_1{std::cref(foo), std::cref(bar),
+                                                std::cref(baz)};
+  const sourcemeta::core::WeakPointer pointer_2{std::cref(foo), std::cref(bar),
+                                                std::cref(baz)};
+  const std::hash<sourcemeta::core::WeakPointer> hasher;
+  EXPECT_EQ(hasher(pointer_1), hasher(pointer_2));
+}
+
+TEST(JSONPointer_hash, weakpointer_different_pointers_different_hash) {
+  const std::string foo = "foo";
+  const std::string bar = "bar";
+  const std::string baz = "baz";
+  const std::string qux = "qux";
+  const sourcemeta::core::WeakPointer pointer_1{std::cref(foo), std::cref(bar)};
+  const sourcemeta::core::WeakPointer pointer_2{std::cref(baz), std::cref(qux)};
+  const std::hash<sourcemeta::core::WeakPointer> hasher;
+  EXPECT_NE(hasher(pointer_1), hasher(pointer_2));
+}
+
+TEST(JSONPointer_hash, weakpointer_unordered_set) {
+  const std::string foo = "foo";
+  const std::string bar = "bar";
+  std::unordered_set<sourcemeta::core::WeakPointer> pointers;
+  pointers.insert(sourcemeta::core::WeakPointer{std::cref(foo)});
+  pointers.insert(sourcemeta::core::WeakPointer{std::cref(bar)});
+  pointers.insert(sourcemeta::core::WeakPointer{std::cref(foo)});
+  EXPECT_EQ(pointers.size(), 2);
+  EXPECT_TRUE(pointers.contains(sourcemeta::core::WeakPointer{std::cref(foo)}));
+  EXPECT_TRUE(pointers.contains(sourcemeta::core::WeakPointer{std::cref(bar)}));
+}
+
+TEST(JSONPointer_hash, weakpointer_unordered_map) {
+  const std::string foo = "foo";
+  const std::string bar = "bar";
+  std::unordered_map<sourcemeta::core::WeakPointer, int> map;
+  map[sourcemeta::core::WeakPointer{std::cref(foo)}] = 1;
+  map[sourcemeta::core::WeakPointer{std::cref(bar)}] = 2;
+  map[sourcemeta::core::WeakPointer{std::cref(foo)}] = 3;
+  EXPECT_EQ(map.size(), 2);
+  EXPECT_EQ(map[sourcemeta::core::WeakPointer{std::cref(foo)}], 3);
+  EXPECT_EQ(map[sourcemeta::core::WeakPointer{std::cref(bar)}], 2);
+}
+
+TEST(JSONPointer_hash, pointer_and_weakpointer_consistent) {
+  const sourcemeta::core::Pointer pointer{"foo", "bar", "baz"};
+  const std::string foo = "foo";
+  const std::string bar = "bar";
+  const std::string baz = "baz";
+  const sourcemeta::core::WeakPointer weak_pointer{
+      std::cref(foo), std::cref(bar), std::cref(baz)};
+  const std::hash<sourcemeta::core::Pointer> pointer_hasher;
+  const std::hash<sourcemeta::core::WeakPointer> weak_pointer_hasher;
+  EXPECT_EQ(pointer_hasher(pointer), weak_pointer_hasher(weak_pointer));
+}
