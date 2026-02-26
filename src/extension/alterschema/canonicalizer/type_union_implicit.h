a7c3dacc0ea768e1e53dfe2f03e6e547a82125db
Staging-Devin AI <166158716+staging-devin-ai-integration[bot]@users.noreply.github.com>
2026-02-26 01:01:57 +0000

    Fix type_union_implicit for draft1 and draft2 enums
    Co-Authored-By: bot_apk <apk@cognition.ai>

--- src/extension/alterschema/canonicalizer/type_union_implicit.h
+++ src/extension/alterschema/canonicalizer/type_union_implicit.h
@@ -98,20 +98,23 @@ public:
       return false;
     }
 
-    if (vocabularies.contains(
-            "http://json-schema.org/draft-02/hyper-schema#") &&
+    if ((vocabularies.contains("http://json-schema.org/draft-02/schema#") ||
+         vocabularies.contains(
+             "http://json-schema.org/draft-02/hyper-schema#")) &&
         schema.defines_any({"enum", "disallow", "extends"})) {
       return false;
     }
 
-    if (vocabularies.contains(
-            "http://json-schema.org/draft-01/hyper-schema#") &&
+    if ((vocabularies.contains("http://json-schema.org/draft-01/schema#") ||
+         vocabularies.contains(
+             "http://json-schema.org/draft-01/hyper-schema#")) &&
         schema.defines_any({"enum", "disallow", "extends"})) {
       return false;
     }
 
-    if (vocabularies.contains(
-            "http://json-schema.org/draft-00/hyper-schema#") &&
+    if ((vocabularies.contains("http://json-schema.org/draft-00/schema#") ||
+         vocabularies.contains(
+             "http://json-schema.org/draft-00/hyper-schema#")) &&
         schema.defines_any({"enum", "disallow", "extends"})) {
       return false;
     }
